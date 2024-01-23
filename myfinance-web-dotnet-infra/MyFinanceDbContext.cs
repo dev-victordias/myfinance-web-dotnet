a921d0523fc4b660c08b9203e7729f97285e6c5d
Victor Dias <victorhugmoraisdias@gmail.com>
2024-01-22 23:27:42 -0300

    configurando os Entities

--- myfinance-web-dotnet-infra/MyFinanceDbContext.cs
+++ myfinance-web-dotnet-infra/MyFinanceDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace myfinance_web_dotnet_infra;
+﻿using Microsoft.EntityFrameworkCore;
+using myfinance_web_dotnet_domain;
 
-public class MyFinanceDbContext
+namespace myfinance_web_dotnet_infra;
+
+public class MyFinanceDbContext : DbContext
 {
 
+    public DbSet<PlanoConta> PlanoConta { get; set; }
+    public DbSet<Transacao> Transacao { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=myfinance;Trusted_Connection=True;");
+    }
 }
